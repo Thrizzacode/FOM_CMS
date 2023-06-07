9676940317f12342cb579b339e545d1a004c4ba5
TpxyFAE <mikework0814@gmail.com>
2023-06-07 17:49:10 +0800

    1.添加公告分頁功能

--- src/views/Announcement.vue
+++ src/views/Announcement.vue
@@ -49,6 +49,12 @@
               />
               <el-table-column align="center" prop="content" label="公告內容" />
               <el-table-column
+                width="100"
+                align="center"
+                prop="publisher"
+                label="公告人"
+              />
+              <el-table-column
                 label="編輯"
                 align="center"
                 width="200px"
@@ -59,13 +65,13 @@
                   <el-button
                     size="small"
                     type="primary"
-                    @click="editMemberConfirm(scope.row)"
+                    @click="editAnnouncementConfirm(scope.row)"
                     >編輯</el-button
                   >
                   <el-button
                     size="small"
                     type="danger"
-                    @click="delMemberConfirm(scope.row)"
+                    @click="delAnnouncementConfirm(scope.row)"
                     >刪除</el-button
                   >
                 </template>
@@ -76,7 +82,6 @@
               <el-col :span="24">
                 <div class="pageination">
                   <el-pagination
-                    v-if="total > 0"
                     class="msg-pagination-container"
                     v-model:current-page="page_index"
                     v-model:page-size="page_size"
@@ -90,21 +95,19 @@
               </el-col>
             </el-row>
           </div>
-          <el-dialog v-model="addDialogVisible" title="新增會員" width="30%">
+          <el-dialog v-model="addDialogVisible" title="新增公告" width="30%">
             <el-form
               ref="ruleFromRef"
               :model="announcementForm"
               :label-position="position"
               :rules="rules"
             >
-              <el-form-item label="會員姓名:" prop="name">
-                <el-input v-model="announcementForm.name" />
-              </el-form-item>
-              <el-form-item label="電話:" prop="phone">
-                <el-input v-model="announcementForm.phone" maxlength="10" />
-              </el-form-item>
-              <el-form-item label="地址:" prop="address">
-                <el-input v-model="announcementForm.address" />
+              <el-form-item label="公告內容:" prop="content">
+                <el-input
+                  type="textarea"
+                  rows="15"
+                  v-model="announcementForm.content"
+                />
               </el-form-item>
             </el-form>
             <template #footer>
@@ -116,27 +119,21 @@
                   "
                   >取消</el-button
                 >
-                <el-button type="primary" @click="addMember(ruleFromRef)">
+                <el-button type="primary" @click="addAnnouncement(ruleFromRef)">
                   確認
                 </el-button>
               </span>
             </template>
           </el-dialog>
-          <el-dialog v-model="editDialogVisible" title="修改會員" width="30%">
+          <el-dialog v-model="editDialogVisible" title="修改公告" width="30%">
             <el-form
               ref="ruleFromRef"
               :model="editIn"
               :label-position="position"
               :rules="rules"
             >
-              <el-form-item label="會員姓名:" prop="name">
-                <el-input v-model="editIn.name" />
-              </el-form-item>
-              <el-form-item label="電話:" prop="phone">
-                <el-input v-model="editIn.phone" maxlength="10" />
-              </el-form-item>
-              <el-form-item label="地址:" prop="address">
-                <el-input v-model="editIn.address" />
+              <el-form-item label="公告內容:" prop="content">
+                <el-input type="textarea" rows="15" v-model="editIn.content" />
               </el-form-item>
             </el-form>
             <template #footer>
@@ -148,7 +145,10 @@
                   "
                   >取消</el-button
                 >
-                <el-button type="primary" @click="editMember(ruleFromRef)">
+                <el-button
+                  type="primary"
+                  @click="editAnnouncement(ruleFromRef)"
+                >
                   確認
                 </el-button>
               </span>
@@ -168,7 +168,7 @@
                 <el-button
                   type="primary"
                   @click="
-                    delMember(deleteIn);
+                    delAnnouncement(deleteIn);
                     delDialogVisible = false;
                   "
                 >
@@ -200,36 +200,38 @@ const delDialogVisible = ref(false);
 const deleteIn = ref({});
 const editIn = ref({});
 const announcementForm = reactive({
-  date: "",
   content: "",
 });
+
+//分頁配置設定
+const total = ref(0); //總共有幾條
+const page_index = ref(1); //現在在哪裡一頁
+const page_size = ref(10); //一頁有多少條
+const page_sizes = [10, 20, 50, 100]; //每頁有幾條
+const layout = "total, sizes, prev, pager, next, jumper"; //下一頁布局
+const pageable = reactive({
+  page: page_index.value - 1,
+  size: page_size.value,
+  sort: "announcementid,desc",
+});
+
+//分頁方法處理
+const handleSizeChange = (size) => {
+  pageable.size = size;
+  fetchAnnouncement();
+};
+const handleCurrentChange = (page) => {
+  pageable.page = page - 1;
+  fetchAnnouncement();
+};
 
 //表單驗證
 const ruleFromRef = ref(null);
 const rules = reactive({
-  name: [
+  content: [
     {
       required: true,
-      message: "請輸入會員姓名",
-      trigger: "blur",
-    },
-  ],
-  phone: [
-    {
-      required: true,
-      message: "請輸入電話",
-      trigger: "blur",
-    },
-    {
-      pattern: /^[0][9][0-9]{8}$/,
-      trigger: ["blur", "change"],
-      message: "請輸入09開頭的正確電話號碼",
-    },
-  ],
-  address: [
-    {
-      required: true,
-      message: "請輸入地址",
+      message: "請輸入公告內容",
       trigger: "blur",
     },
   ],
@@ -239,23 +241,45 @@ const resetInputForm = (formEl) => {
   formEl.resetFields();
 };
 
-//獲取會員資料
+//獲取公告資料
 const fetchAnnouncement = async () => {
+  console.log(pageable);
   try {
-    const { data } = await axios.get(import.meta.env.VITE_GET_ANNOUNCEMENT_API);
-    announcementData.value = data;
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_GET_ANNOUNCEMENT_API}?page=${
+        page_index.value - 1
+      }&size=${page_size.value}&sort=announcementid,desc`,
+      pageable
+    );
+    console.log(data);
+    announcementData.value = data.content;
     announcementData.value.map((item) => {
       item.date = useDateFormat(item.date, "YYYY-MM-DD HH:mm:ss");
     });
-    // setPageinations();
+    total.value = data.totalElements;
   } catch (error) {
     console.log(error);
   }
 };
-onMounted(fetchAnnouncement);
 
-//新增會員
-const addMember = (formEl) => {
+const fetchAnnouncementCount = async () => {
+  try {
+    const { data } = await axios.get(
+      import.meta.env.VITE_GET_ANNOUNCEMENT_COUNT_API
+    );
+    total.value = data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+onMounted(() => {
+  fetchAnnouncement();
+  fetchAnnouncementCount();
+});
+
+//新增公告
+const addAnnouncement = (formEl) => {
   if (!formEl) return;
   formEl.validate(async (valid, fields) => {
     if (valid) {
@@ -263,12 +287,13 @@ const addMember = (formEl) => {
       try {
         console.log(announcementForm);
         const { data } = await axios.post(
-          import.meta.env.VITE_ADD_MEMBERSHIP_API,
+          import.meta.env.VITE_ADD_ANNOUNCEMENT_API,
           announcementForm
         );
 
         resetForm();
-        resetData();
+        fetchAnnouncement();
+        fetchAnnouncementCount();
         addDialogVisible.value = false;
         ElMessage({
           type: "success",
@@ -284,15 +309,15 @@ const addMember = (formEl) => {
   });
 };
 
-//修改會員
-const editMember = (formEl) => {
+//修改公告
+const editAnnouncement = (formEl) => {
   if (!formEl) return;
   formEl.validate(async (valid, fields) => {
     if (valid) {
       console.log("submit!");
       try {
         const { data } = await axios.put(
-          import.meta.env.VITE_EDIT_MEMBERSHIP_API,
+          import.meta.env.VITE_EDIT_ANNOUNCEMENT_API,
           editIn.value
         );
         console.log(data);
@@ -313,12 +338,14 @@ const editMember = (formEl) => {
   });
 };
 
-//刪除會員
-const delMember = async (deleteIn) => {
+//刪除公告
+const delAnnouncement = async (deleteIn) => {
   console.log(deleteIn);
   try {
     const { data } = await axios.delete(
-      `${import.meta.env.VITE_DELETE_MEMBERSHIP_API}${deleteIn.id}`
+      `${import.meta.env.VITE_DELETE_ANNOUNCEMENT_API}${
+        deleteIn.announcementid
+      }`
     );
     console.log(data);
     resetData();
@@ -331,15 +358,16 @@ const delMember = async (deleteIn) => {
   }
 };
 
-//確認是否刪除會員
-const delMemberConfirm = (row) => {
+//確認是否刪除公告
+const delAnnouncementConfirm = (row) => {
   console.log(row);
   delDialogVisible.value = true;
   deleteIn.value = row;
   console.log(deleteIn.value);
 };
 
-const editMemberConfirm = (row) => {
+//確認是否修改公告
+const editAnnouncementConfirm = (row) => {
   console.log(row);
   editDialogVisible.value = true;
   editIn.value = row;
@@ -348,36 +376,13 @@ const editMemberConfirm = (row) => {
 
 //重置Form
 const resetForm = () => {
-  announcementForm.name = "";
-  announcementForm.phone = "";
-  announcementForm.address = "";
-};
-
-//重置資料
-const resetData = async () => {
-  try {
-    const { data } = await axios.get(import.meta.env.VITE_GET_MEMBERSHIP_API);
-    console.log(data);
-    announcementData.value = data;
-    console.log(announcementData.value);
-    setPageinations();
-  } catch (error) {
-    console.log(error);
-  }
+  announcementForm.content = "";
 };
 
 //重新整理table
 const refreshPage = () => {
-  resetData();
-  console.log("done");
+  fetchAnnouncement();
 };
-//分頁配置設定
-const page_index = ref(1), //現在在哪裡一頁
-  page_size = ref(5), //一頁有多少條
-  total = ref(0), //有幾條
-  page_sizes = [10, 20, 50, 100], //每頁有幾條
-  layout = "total, sizes, prev, pager, next, jumper", //下一頁布局
-  isBackground = true;
 
 //查詢功能設定
 const startTime = ref();
@@ -385,92 +390,6 @@ const endTime = ref();
 const productTypeValue = ref();
 const statusValue = ref();
 const membershipID = ref();
-
-//表格資訊
-// const getProfiles = async () => {
-//   const { data } = await axios.get(import.meta.env.VITE_GET_WORKLOG_API!);
-//   console.log(data);
-//   announcementData.value = data.reverse();
-//   allTableData.value = data;
-//   filterTableData.value = data;
-//   setPageinations();
-// };
-//一進來就render表格
-// watchEffect(() => getProfiles());
-// const handleContent = (row) => {
-//   showr.value = true;
-//   console.log(row);
-//   contentData.value = row;
-// };
-// const handleDetailContent = (row, column) => {
-//   showd.value = true;
-//   console.log(row);
-//   console.log(column);
-//   rowData.value = row;
-//   columnData.value = column;
-// };
-//編輯按鈕
-const handleEdit = (row) => {
-  show.value = true;
-  console.log(row);
-  editData.value = row;
-};
-//刪除按鈕
-// const handleDelete = async (row) => {
-//   centerDialogVisible.value = true;
-//   deleteIn = row;
-//   getProfiles();
-//   return deleteIn;
-// };
-//刪除按鈕內部
-// const handleDeleteIn = (deleteIn) => {
-//   axios.delete(`/api/worklog/deleteworklog/${deleteIn._id}`);
-//   ElMessage.success("刪除成功");
-//   getProfiles();
-// };
-//新增按鈕
-const orderAdd = () => {
-  show.value = true;
-  editData.value = "";
-};
-//重新render表格
-// const handleUpdateProfiles = () => {
-//   getProfiles();
-// };
-//下載圖片
-const download = (file) => {
-  let a = document.createElement("a");
-  a.download = file.column.label;
-  a.href = file.row.base;
-  a.click();
-};
-
-//分頁方法處理
-const handleSizeChange = (pages) => {
-  // console.log(pages);
-  page_index.value = 1;
-  page_size.value = pages;
-  announcementData.value = allTableData.value.filter((item, index) => {
-    return index < page_size.value;
-  });
-};
-const handleCurrentChange = (page) => {
-  let currentPage = page_size.value * (page - 1);
-  let pageData = allTableData.value.filter((item, index) => {
-    return index >= currentPage;
-  });
-  announcementData.value = pageData.filter((item, index) => {
-    return index < page_size.value;
-  });
-};
-const setPageinations = () => {
-  total.value = allTableData.value.length;
-  page_index.value = 1;
-  page_size.value = 10;
-  announcementData.value = allTableData.value.filter((item, index) => {
-    return index < page_size.value;
-  });
-};
 
 //查詢按鈕
 const sort = () => {
@@ -518,69 +437,6 @@ const sort = () => {
   //選擇後數據重新分頁
   setPageinations();
 };
-//下載選擇內容EXCEL
-// const downloadDetail = () => {
-//   const workbook = new Workbook();
-//   const worksheet = workbook.addWorksheet("交接列表");
-//   worksheet.columns = [
-//     { header: "建單時間", key: "createDate", width: 20 },
-//     { header: "單號", key: "caseNumber", width: 20 },
-//     { header: "提交人", key: "receiver", width: 20 },
-//     { header: "產品別", key: "productType", width: 20 },
-//     { header: "商戶名稱", key: "merchant", width: 20 },
-//     { header: "案件分類第一層", key: "caseTypeFirst", width: 20 },
-//     { header: "案件分類第二層", key: "caseTypeSecond", width: 20 },
-//     { header: "案件分類第三層", key: "caseTypeThird", width: 20 },
-//     { header: "案件描述", key: "describe", width: 20 },
-//     { header: "案件回復", key: "caseRes", width: 20 },
-//     { header: "處理單位", key: "workUnit", width: 20 },
-//     { header: "案件狀況", key: "caseStatus", width: 20 },
-//     { header: "結案時間", key: "caseOverTime", width: 20 },
-//   ];
-//   const ptvalue = productTypeValue.value ? productTypeValue.value : null;
-//   const caseStatus = statusValue.value ? statusValue.value : null;
-//   const serialNum = membershipID.value ? membershipID.value : null;
-//   const stime = startTime.value ? startTime.value.getTime() : null;
-//   const etime = endTime.value ? endTime.value.getTime() : null;
-//   allTableData.value = filterTableData.value.filter((item) => {
-//     let date = new Date(item.date);
-//     let time = date.getTime();
-//     return (
-//       (stime ? time >= stime : true) &&
-//       (etime ? time <= etime : true) &&
-//       (ptvalue ? item.productType === ptvalue : true) &&
-//       (caseStatus ? item.caseStatus === caseStatus : true) &&
-//       (serialNum ? item.caseNumber === serialNum : true)
-//     );
-//   }); //
-//   // console.log(allTableData.value);
-//   allTableData.value.forEach((item{
-//     worksheet.addRow({
-//       createDate: item.creatDate,
-//       caseNumber: item.caseNumber,
-//       receiver: item.receiver,
-//       productType: item.productType,
-//       merchant: item.merchant,
-//       caseTypeFirst: item.caseTypeFirst,
-//       caseTypeSecond: item.caseTypeSecond,
-//       caseTypeThird: item.caseTypeThird,
-//       describe: item.describe,
-//       caseRes: item.caseRes,
-//       workUnit: item.workUnit,
-//       caseStatus: item.caseStatus,
-//       caseOverTime: item.caseOverTime,
-//     });
-//   });
-//   workbook.xlsx.writeBuffer().then((buffer) => {
-//     const link = document.createElement("a");
-//     const blobData = new Blob([buffer], {
-//       type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-//     });
-//     link.download = "交接試算表.xlsx";
-//     link.href = window.URL.createObjectURL(blobData);
-//     link.click();
-//   });
-// };
 </script>
 
 <style lang="scss" scoped>
