676b4685345e144ffa94a9f7db0599392e6f5bf9
TpxyFAE <mikework0814@gmail.com>
2023-06-08 10:05:41 +0800

    1.router修改HashRouter

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "/FOM_CMS/",
+  base: "./",
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
